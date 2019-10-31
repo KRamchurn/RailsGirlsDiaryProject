8fb6798586e051378ac3b6efd82c8d08e33611b8
Kunal Ramchurn <kunalsingh.ramchurn@postgrad.manchester.ac.uk>
2019-10-31 12:50:33 +0000

    initial commit

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,33 @@
+class EntriesController < ApplicationController
+
+  def index
+      @entries = Entry.all
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    entry_params = params["entry"].permit("title", "contents")
+    render(:text => params.inspect)
+    redirect_to(entry_path(entry))
+  end
+
+  def edit
+    @entry = Entry.find(params["id"])
+  end
+
+  def update
+  entry_params = params["entry"].permit("title", "contents")
+  entry = Entry.find(params["id"])
+  entry.update(entry_params)
+  redirect_to(entry_path(entry))
+end
+
+
+end
