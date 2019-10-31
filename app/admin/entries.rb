483cf1440a2d4340044d6649dee0c85491983c4d
Kunal Ramchurn <kunalsingh.ramchurn@postgrad.manchester.ac.uk>
2019-10-31 13:27:05 +0000

    added activeadmin

---
+++ app/admin/entries.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register Entry do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :date, :contents
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :date, :contents]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  permit_params :title, :description, :picture
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'welcome#index'
 
   resources "entries"
+  #resources :entries, only [:show, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
